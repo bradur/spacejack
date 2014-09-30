a21451867980a69cbf4eacbcd044ea6fb86f5c59
skottilaine <matti_tahvanainen@hotmail.com>
2014-09-30 21:45:37 +0300

    player can move between home and an asteroid

---
+++ SpaceMiner/Assets/scripts/game/Asteroid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour
+{
+		public GameObject player;
+		Player p;
+		// Use this for initialization
+		void Start ()
+		{
+
+				p = player.GetComponent<Player> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+		
+		void OnMouseDrag ()
+		{
+				p.MoveTo (transform.position);
+		}
+
+}
---
+++ SpaceMiner/Assets/scripts/game/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+		public float speed;
+		// Use this for initialization
+		bool onJourney;
+		Vector2 travelingPoint;
+
+		void Start ()
+		{
+				onJourney = false;
+		}
+
+		void Update ()
+		{
+				if (onJourney) {
+						transform.position = Vector2.MoveTowards (transform.position, travelingPoint, speed * Time.deltaTime);
+						if (transform.position.x == this.travelingPoint.x && transform.position.y == this.travelingPoint.y) {
+								onJourney = false;
+						}
+				}
+		}
+
+		public void MoveTo (Vector3 v)
+		{
+				if (!onJourney) {
+						this.onJourney = true;
+						this.travelingPoint = v;
+				}
+				
+		}
+}
