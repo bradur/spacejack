09c53de8742ebbfcbde14a4b4812413712714c4a
unknown <bradurdung@gmail.com>
2014-09-30 20:56:04 +0300

    Menu eye candy.

---
+++ SpaceMiner/Assets/MenuPlanet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPlanet : MonoBehaviour {
+
+	public float RotationStep = 0.2f;
+    Vector3 eulerAngles;
+
+    // Use this for initialization
+	void Start () {
+        eulerAngles = new Vector3(0f, 0f, RotationStep);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(eulerAngles);
+	}
+}
