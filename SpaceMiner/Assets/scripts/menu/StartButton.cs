a646bfc8d4f4726018c2d1b2e57fa6b905d9ed79
skottilaine <matti_tahvanainen@hotmail.com>
2014-10-04 15:48:16 +0300

    added start button to menu which opens game scene

---
+++ SpaceMiner/Assets/scripts/menu/StartButton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour
+{
+		Color originalColor;
+		public Color hoverColor;
+		public Color activeColor;
+		SpriteRenderer sr;
+		// Use this for initialization
+		void Start ()
+		{
+				sr = GetComponent<SpriteRenderer> ();
+				originalColor = sr.color;
+		}
+
+		void OnMouseEnter ()
+		{
+				sr.color = hoverColor;
+		}
+
+		void OnMouseExit ()
+		{
+				sr.color = originalColor;
+		}
+
+		void OnMouseUp ()
+		{
+				sr.color = originalColor;
+				Application.LoadLevel (1);
+		}
+
+		void OnMouseDown ()
+		{
+				sr.color = activeColor;
+		}
+
+		void OnMouseDrag ()
+		{
+				
+		}
+}
