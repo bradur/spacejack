64c46603f5f3ed35cf1886c137ffc42835227266
skottilaine <matti_tahvanainen@hotmail.com>
2014-10-05 01:20:04 +0300

    initialized asteroid instantioning

---
+++ SpaceMiner/Assets/scripts/game/AsteroidManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidManager : MonoBehaviour
+{
+
+		Object asteroidTemplate;
+		GameObject tempAsteroid;
+
+		void Start ()
+		{
+				asteroidTemplate = Resources.Load ("asteroidpref");				
+				GenerateAsteroid ();
+
+		}
+
+		void Update ()
+		{
+	
+		}
+
+		public void GenerateAsteroid ()
+		{
+				tempAsteroid = (GameObject)Instantiate (asteroidTemplate);
+				tempAsteroid.transform.position = new Vector2 (2, 1);
+		}
+}
