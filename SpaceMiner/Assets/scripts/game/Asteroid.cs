ddaed263480d220a784837e7d243e5c87a1b8238
unknown <bradurdung@gmail.com>
2014-10-04 17:10:35 +0300

    Game music and Asteroid rotation.

--- SpaceMiner/Assets/scripts/game/Asteroid.cs
+++ SpaceMiner/Assets/scripts/game/Asteroid.cs
@@ -1,26 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class Asteroid : MonoBehaviour
 {
+		public float MaxRotationStep = 0.2f;
+		public float MinRotationStep = 0.05f;
+		float RotationStep;
+    	Vector3 eulerAngles;
 		public GameObject player;
 		Player p;
 		// Use this for initialization
 		void Start ()
 		{
-
-				p = player.GetComponent<Player> ();
+			RotationStep = Random.Range(MinRotationStep, MaxRotationStep);
+			int rotdir = Random.value > 0.5f ? 1 : -1;
+			RotationStep = RotationStep * rotdir;
+			eulerAngles = new Vector3(0f, 0f, RotationStep);
+			p = player.GetComponent<Player> ();
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-	
+			transform.Rotate(eulerAngles);
 		}
 		
 		void OnMouseDrag ()
 		{
-				p.MoveTo (transform.position);
+			p.MoveTo (transform.position);
 		}
 
 		
