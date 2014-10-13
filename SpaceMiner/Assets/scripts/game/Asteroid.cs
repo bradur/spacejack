313c9630f35b683b1deba7919003741cf0e17108
skottilaine <matti_tahvanainen@hotmail.com>
2014-10-13 19:28:05 +0300

    asteroids have mineral amount

--- SpaceMiner/Assets/scripts/game/Asteroid.cs
+++ SpaceMiner/Assets/scripts/game/Asteroid.cs
@@ -3,41 +3,45 @@ using System.Collections;
 
 public class Asteroid : MonoBehaviour
 {
-    public float MaxRotationStep = 0.2f;
-    public float MinRotationStep = 0.05f;
-    float RotationStep;
-    Vector3 eulerAngles;
-    public GameObject playerObject;
-    Player player;
-    bool collided = false;
-    // Use this for initialization
-    void Start ()
-    {
-        RotationStep = Random.Range(MinRotationStep, MaxRotationStep);
-        int rotdir = Random.value > 0.5f ? 1 : -1;
-        RotationStep = RotationStep * rotdir;
-        eulerAngles = new Vector3(0f, 0f, RotationStep);
-        player = playerObject.GetComponent<Player>();
-    }
+		public float MaxRotationStep = 0.2f;
+		public float MinRotationStep = 0.05f;
+		float RotationStep;
+		Vector3 eulerAngles;
+		public GameObject playerObject;
+		Player player;
+		bool collided = false;
+		int mineralAmount;
 
-    public void DestroySelf(){
-        // animation ? 
-        Destroy(gameObject);
-    }
+		// Use this for initialization
+		void Start ()
+		{
+				RotationStep = Random.Range (MinRotationStep, MaxRotationStep);
+				int rotdir = Random.value > 0.5f ? 1 : -1;
+				RotationStep = RotationStep * rotdir;
+				eulerAngles = new Vector3 (0f, 0f, RotationStep);
+				player = playerObject.GetComponent<Player> ();
+				mineralAmount = transform.localScale * 1000;
+		}
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Rotate(eulerAngles);
-    }
+		public void DestroySelf ()
+		{
+				// animation ? 
+				Destroy (gameObject);
+		}
 
-    void OnMouseUp()
-    {
-        player.MoveTo(transform.position);
-    }
+		// Update is called once per frame
+		void Update ()
+		{
+				transform.Rotate (eulerAngles);
+		}
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        collided = true;
-    }
+		void OnMouseUp ()
+		{
+				player.MoveTo (transform.position);
+		}
+
+		void OnTriggerEnter2D (Collider2D col)
+		{
+				collided = true;
+		}
 }
