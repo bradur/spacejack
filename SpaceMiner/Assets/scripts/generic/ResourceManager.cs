383e7f1cc120f44fb964b1ce97529e0f4e24f127
unknown <bradurdung@gmail.com>
2014-10-22 23:31:12 +0300

    Market now gets values from ResourceManager and displays them accordingly.

--- SpaceMiner/Assets/scripts/generic/ResourceManager.cs
+++ SpaceMiner/Assets/scripts/generic/ResourceManager.cs
@@ -1,65 +1,77 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ResourceManager : MonoBehaviour
-{       
-		public int dmitryivanoviteAmount;
-		public int sinoiteAmount;
-		public int fuelAmount;
-		public int maxFuelAmount;
-
-		void Awake ()
-		{
-				DontDestroyOnLoad (transform.gameObject);
-		}
-
-		void Start ()
-		{
-
-		}
-
-		void Update ()
-		{
-            
-		}
-
-		public void GiveMinerals (Mineral mineralType, int amount)
-		{
-				switch (mineralType) {
-				case Mineral.Dmitryivanovite:
-						dmitryivanoviteAmount += amount;
-						break;
-        
-				case Mineral.Sinoite:
-						sinoiteAmount += amount;
-						break;
-				}
-		}
-
-		public int FuelAmount {
-				set{ this.fuelAmount = value;}
-				get{ return this.fuelAmount;}
-		}
-}
-
 public enum Mineral
 {
-		Sinoite,
-		Dmitryivanovite,
-		Alabandite}
-;
+    Sinoite,
+    Dmitryivanovite,
+    Alabandite
+};
 
 public enum Tool
 {
-		BronzePickaxe,
-		DiamondPickaxe,
-		Drill,
-		Laser}
-;
+    BronzePickaxe,
+    DiamondPickaxe,
+    Drill,
+    Laser
+};
 
 public enum JetPackUpgrade
 {
-		Lazy,
-		TheBeast,
-		Expert
+    Lazy,
+    TheBeast,
+    Expert
 }
+
+public class ResourceManager : MonoBehaviour
+{
+    public int fuelAmount;
+    public int maxFuelAmount;
+
+    public int creditCount;
+
+    public int[] mineralArray = new int[System.Enum.GetNames(typeof(Mineral)).Length];
+
+    void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    void Start ()
+    {
+    }
+
+    void Update ()
+    {
+        
+    }
+
+    public int GetMineralCount(Mineral mineral){
+        return mineralArray[(int)mineral];
+    }
+
+    public void SetMineralCount(Mineral mineral, int count){
+        mineralArray[(int)mineral] += count;
+    }
+
+    public void GiveMinerals (Mineral mineralType, int amount)
+    {
+        mineralArray[(int)mineralType] += amount;
+        /*
+        switch (mineralType) {
+        case Mineral.Dmitryivanovite:
+            dmitryivanoviteAmount += amount;
+            break;
+
+        case Mineral.Sinoite:
+            sinoiteAmount += amount;
+            break;
+        }*/
+    }
+
+    public int FuelAmount {
+        set{ this.fuelAmount = value;}
+        get{ return this.fuelAmount;}
+    }
+}
+
+
---
+++ SpaceMiner/Assets/scripts/market/MarketManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarketManager : MonoBehaviour {
+
+    ResourceManager resourceManager;
+
+    public GameObject fuelCountObject;
+    TextMesh fuelCountText;
+    int fuelCount;
+    string maxFuel;
+
+    public GameObject creditCountObject;
+    TextMesh creditCountText;
+    int creditCount;
+
+    public int[] mineralArray = new int[System.Enum.GetNames(typeof(Mineral)).Length];
+    public GameObject[] mineralObjects = new GameObject[System.Enum.GetNames(typeof(Mineral)).Length];
+    TextMesh[] mineralTexts = new TextMesh[System.Enum.GetNames(typeof(Mineral)).Length];
+
+
+    // Use this for initialization
+    void Start () {
+        resourceManager = GameObject.FindWithTag("resourceManager").GetComponent<ResourceManager>();
+        fuelCountText = fuelCountObject.GetComponent<TextMesh>();
+        creditCountText = creditCountObject.GetComponent<TextMesh>();
+
+        //resourceManager.GetMineralCount(Mineral.Sinoite);
+        //resourceManager.GetMineralCount(Mineral.Dmitryivanovite);
+        //resourceManager.GetMineralCount(Mineral.Alabandite);
+
+        for(int i = 0;i < mineralArray.Length; i++){
+            mineralArray[i] = resourceManager.GetMineralCount((Mineral)i);
+        }
+        for(int i = 0; i< mineralObjects.Length; i++){
+            mineralTexts[i] = mineralObjects[i].GetComponent<TextMesh>();
+            UpdateMineralCount((Mineral)i);
+        }
+
+        creditCount = resourceManager.creditCount;
+        fuelCount = resourceManager.fuelAmount;
+        maxFuel = resourceManager.maxFuelAmount.ToString();
+        UpdateCreditCount();
+        UpdateFuelCount();
+
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    public void UpdateMineralCount(Mineral mineral, int count=0){
+        mineralArray[(int)mineral] += count;
+        resourceManager.SetMineralCount(mineral, count);
+        print(mineral + ": " + resourceManager.GetMineralCount(mineral));
+        mineralTexts[(int)mineral].text = resourceManager.GetMineralCount(mineral).ToString();
+    }
+
+    public void UpdateCreditCount(int newCount=0){
+        creditCount += newCount;
+        creditCountText.text = "$" + creditCount.ToString();
+        resourceManager.creditCount = creditCount;
+    }
+
+    public void UpdateFuelCount(int newCount=0){
+        fuelCount += newCount;
+        fuelCountText.text = fuelCount.ToString() + " / " + maxFuel;
+        print(maxFuel);
+        resourceManager.fuelAmount = fuelCount;
+    }
+}
