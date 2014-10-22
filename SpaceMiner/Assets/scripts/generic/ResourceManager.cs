a6e42aaa5d713592a66225a7853f1a83a809adb8
unknown <bradurdung@gmail.com>
2014-10-22 21:31:35 +0300

    ResourceManager now persists between scenes.

--- SpaceMiner/Assets/scripts/generic/ResourceManager.cs
+++ SpaceMiner/Assets/scripts/generic/ResourceManager.cs
@@ -2,60 +2,63 @@
 using System.Collections;
 
 public class ResourceManager : MonoBehaviour
-{		
-		public int dmitryivanoviteAmount;
-		public int sinoiteAmount;
-		public int fuelAmount;
+{       
+    public int dmitryivanoviteAmount;
+    public int sinoiteAmount;
+    public int fuelAmount;
 
-		void Start ()
-		{
 
-		}
+    void Awake () {
+        DontDestroyOnLoad(transform.gameObject);
+    }
 
-		void Update ()
-		{
-				
-		}
+    void Start ()
+    {
 
-		public void GiveMinerals (Mineral mineralType, int amount)
-		{
-				switch (mineralType) {
-				case Mineral.Dmitryivanovite:
-						dmitryivanoviteAmount += amount;
-						break;
-				
-				case Mineral.Sinoite:
-						sinoiteAmount += amount;
-						break;
-				}
-		}
+    }
 
-		public int FuelAmount {
-				set{ this.fuelAmount = value;}
-				get{ return this.fuelAmount;}
-		}
+    void Update ()
+    {
+            
+    }
+
+    public void GiveMinerals (Mineral mineralType, int amount)
+    {
+        switch (mineralType) {
+        case Mineral.Dmitryivanovite:
+            dmitryivanoviteAmount += amount;
+            break;
+        
+        case Mineral.Sinoite:
+            sinoiteAmount += amount;
+            break;
+        }
+    }
+
+    public int FuelAmount {
+        set{ this.fuelAmount = value;}
+        get{ return this.fuelAmount;}
+    }
 }
 
 public enum Mineral
 {
-		Sinoite,
-		Dmitryivanovite,
-		Alabandite}
+    Sinoite,
+    Dmitryivanovite,
+    Alabandite}
 ;
 
 public enum Tool
 {
-		BronzePickaxe,
-		DiamondPickaxe,
-		Drill,
-		Laser}
+    BronzePickaxe,
+    DiamondPickaxe,
+    Drill,
+    Laser}
 ;
 
 public enum JetPackUpgrade
 {
-		Lazy,
-		TheBeast,
-		Expert
-
-
+    Lazy,
+    TheBeast,
+    Expert
 }
