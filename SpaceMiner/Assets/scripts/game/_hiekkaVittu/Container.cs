77a015c6a78889fcdaeb81b609e7313f914ba42d
skottilaine <matti_tahvanainen@hotmail.com>
2014-11-09 22:09:24 +0200

    Matti's mining game skeleton

---
+++ SpaceMiner/Assets/scripts/game/_hiekkaVittu/Container.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Container : MonoBehaviour
+{
+
+		public bool containsMineral;
+
+		// Use this for initializationF
+		void Start ()
+		{
+				int random = Random.Range (0, 3);
+				containsMineral = random == 0 ? true : false;	
+		}
+
+}
---
+++ SpaceMiner/Assets/scripts/game/_hiekkaVittu/TableGenerator.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TableGenerator : MonoBehaviour
+{
+
+//		public Transform element;
+		Object elementTemplate;
+		GameObject tableElement;
+		public int rows;
+		public int columns;
+
+		
+
+		
+
+		// Use this for initialization
+		void Start ()
+		{
+				elementTemplate = Resources.Load ("table_element");
+				
+				GenerateTable ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void GenerateTable ()
+		{
+				float original_x = transform.position.x;
+				float x = transform.position.x;
+				float y = transform.position.y;
+				for (int i = 0; i < rows; i++) {
+						for (int j = 0; j < columns; j++) {
+								GenerateElement (x, y);
+								x += 0.35f;
+						}
+						//how do I count image width/height in here?
+						y -= 0.35f;
+						x = original_x;
+				}
+		}
+
+		void GenerateElement (float x, float y)
+		{
+				tableElement = (GameObject)Instantiate (elementTemplate);
+				tableElement.transform.parent = transform;
+				tableElement.transform.localPosition = new Vector3 (x, y, 0);
+		}
+}
