d0dd2d4a9490e599ea08fa1f95ac017cf63555f8
unknown <bradurdung@gmail.com>
2014-11-09 22:51:48 +0200

    First commit. Basic generating functionality & hover/active states for the squares.

---
+++ SpaceMiner/Assets/GridSquare.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridSquare : MonoBehaviour {
+
+    public Color hoverColor;
+    public Color activeColor;
+
+    SpriteRenderer sr;
+    Color originalColor;
+    // Use this for initialization
+    void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        print(sr.sprite.rect.width);
+        print(sr.sprite.bounds.size);
+        originalColor = sr.color;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    void OnMouseEnter(){
+        sr.color = hoverColor;
+    }
+
+    void OnMouseExit(){
+        sr.color = originalColor;
+    }
+
+    void OnMouseUp(){
+        sr.color = originalColor;
+    }
+
+    void OnMouseDown(){
+        sr.color = activeColor;
+    }
+}
---
+++ SpaceMiner/Assets/scripts/game/miningMiniGame/GridManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridManager : MonoBehaviour {
+
+    // editor variables
+
+    public int columns = 5;
+    public int rows = 5;
+
+    public GameObject background;
+
+
+    SpriteRenderer sr;
+    Object squarePrefab;
+    //float PixelsToUnits;
+
+    void Awake(){
+        squarePrefab = Resources.Load("gridSquare");
+        GenerateGrid();
+    }
+
+    // Use this for initialization
+    void Start () {
+        float moveX = columns/2;
+        float moveY = rows/2;
+        sr = background.GetComponent<SpriteRenderer>();
+        //PixelsToUnits = sr.sprite.rect.width / sr.sprite.bounds.size.x;
+        background.transform.localPosition = new Vector3(moveX, moveY);
+        //float viewPortUnits = Camera.main.orthographicSize*2f*(Screen.width/Screen.height);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    GameObject GenerateSquare(){
+        GameObject newSquare = (GameObject)Instantiate(squarePrefab);
+        newSquare.transform.parent = transform;
+        return newSquare;
+    }
+
+    void GenerateGrid(){
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                GameObject square = GenerateSquare();
+                square.transform.localPosition = new Vector3(1f*j, 1f*i, 0f);
+            }
+        }
+    }
+}
