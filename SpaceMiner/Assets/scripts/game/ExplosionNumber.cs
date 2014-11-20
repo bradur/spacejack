45061af4896cff3f36beb47bc78a99105bbf5bba
skottilaine <matti_tahvanainen@hotmail.com>
2014-11-20 20:51:55 +0200

    Added pictures & animation for explosion countdown

---
+++ SpaceMiner/Assets/scripts/game/ExplosionNumber.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionNumber : MonoBehaviour {
+
+	public float timer;
+	bool countingStarted;
+
+	// Use this for initialization
+	void Start () {
+//		this.GetComponent<Animator>().
+		this.GetComponent<Animator>().speed = 1/12f;
+//		Debug.Log(this.GetComponent<Animator>().speed);
+		
+		countingStarted = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(countingStarted){
+			timer -= Time.deltaTime;
+			if(timer > 2){
+
+			}
+			else if(timer > 1 && timer < 2){
+
+			}
+			else if(timer < 1){
+
+			}
+		}
+	}
+
+	public void StartCounting(){
+		countingStarted = true;
+	}
+}
