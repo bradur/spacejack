9ecf1453b59530abf40f508181eb549518a5f492
skottilaine <matti_tahvanainen@hotmail.com>
2014-11-23 22:28:47 +0200

    made fixes to explosion timer

--- SpaceMiner/Assets/scripts/game/Explosion.cs
+++ SpaceMiner/Assets/scripts/game/Explosion.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class Explosion : MonoBehaviour
 {		
+//		public float timer;
+
 		void Start ()
 		{
-				this.GetComponent<Animator> ().speed = 1 / 12f;				
+				this.GetComponent<Animator> ().speed = 1 / 12f;	
+				Explode (transform.position.x, transform.position.y);
 		}
+
+//		void Update ()
+//		{
+//				timer -= Time.deltaTime;
+//		}
 
 		public void Explode (float x, float y)
 		{
